b0c0f29a7a3f8e2afb0f5e29a2b5f62b77099004
Ksy7sky <nemazenko95@mail.ru>
2021-11-02 22:06:24 +0300

    Dic_pic

--- test.py
+++ test.py
@@ -1,34 +1,8 @@
-slaves = {
-    'student1' : {
-        "Имя" : "Катя",
-        "Возраст" : "15",
-        "Средняя оценка" : "4.6"
-    },
-    'student2' : {
-        "Имя" : "Коля",
-        "Возраст" : "16",
-        "Средняя оценка" : "3.2"
-    },
-    'student3' : {
-        "Имя" : "Петр",
-        "Возраст" : "15",
-        "Средняя оценка" : "4.8"
-    },
-    'student4' : {
-        "Имя" : "Маша",
-        "Возраст" : "16",
-        "Средняя оценка" : "4.9"
-    },
-}
+d = {"Один" : "Питон", "Два" : "C++", "Три" : "Java", "Четыре" : "C#"}
 
-# for key, value in slaves.items():
-#     for key2, value2 in value.items():
-#         print(key2, " - ", value2)
+t = d.copy()
+d.clear()
+t.pop("Три")
 
-def getSlave(name):
-    for key, value in slaves.items():
-        if value["Имя"] == name:
-            for key2, value2 in value.items():
-                print(key2, " - ", value2)
-
-getSlave("Коля")
+t.update({"Новое" : "Kotlin"})
+print(t)
